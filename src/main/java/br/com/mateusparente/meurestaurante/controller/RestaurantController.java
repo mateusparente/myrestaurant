092d8642940e4f0ff720a3ab01f47a6170272e12
Mateus Parente <mpripardo@gmail.com>
2018-07-15 23:48:53 -0300

    error response

--- src/main/java/br/com/mateusparente/meurestaurante/controller/RestaurantController.java
+++ src/main/java/br/com/mateusparente/meurestaurante/controller/RestaurantController.java
@@ -1,12 +1,20 @@
 package br.com.mateusparente.meurestaurante.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.mateusparente.meurestaurante.model.Restaurant;
@@ -25,8 +33,16 @@ public class RestaurantController {
 	}
 	
 	@PostMapping
-	public void save(@RequestBody final Restaurant restaurant){
+	public void save(@RequestBody @Valid final Restaurant restaurant){
 		repository.save(restaurant);
 	}
 	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public List<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+	    return ex.getBindingResult()
+	        .getAllErrors().stream()
+	        .map(ObjectError::getDefaultMessage)
+	        .collect(Collectors.toList());
+	}
 }
